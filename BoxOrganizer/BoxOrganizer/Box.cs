2d2485b6b7653f2930ba08b1bc940483d904bebc
Jimmy <mistcurve@outlook.com>
2019-11-04 13:19:25 -0500

    Added Changes

---
+++ BoxOrganizer/BoxOrganizer/Box.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOrganizer
+{
+    class Box : IComparable
+    {
+        int priority;
+        string[] items;
+        int boxNumber;
+
+        public Box(int priority, int boxNumber, string items)
+        {
+            this.priority = priority;
+            this.boxNumber = boxNumber;
+            this.items = items.Split(' ');
+        }
+        public int CompareTo(object obj)
+        {
+            Box otherBox = (Box)obj;
+            return this.priority - otherBox.priority;
+        }
+        public bool ContainsItem(string wantedItem)
+        {
+            foreach(string item in items)
+            {
+                if (item == wantedItem)
+                    return true;
+            }
+            return false;
+        }
+        public string toString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(this.boxNumber + " ");
+            sb.Append(this.priority + " ");
+            sb.Append("Items[");
+            foreach (string item in items)
+            {
+                sb.Append(item + " ");
+            }
+            sb.Append("]\n");
+            return sb.ToString();
+        }
+    }
+}
--- BoxOrganizer/BoxOrganizer/Program.cs
+++ BoxOrganizer/BoxOrganizer/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
-
+using System.IO;
 namespace BoxOrganizer
 {
     class Program
     {
+        public static string MENU = "1. Add a Box\n2. Search Boxes\n3. Browse Boxes\n4. Quit\n\n";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //System.IO.StreamWriter file = new System.IO.StreamWriter(@"AllBoxes.txt");
+            File.ReadAllLines();
+            int response;
+            do
+            {
+                Console.WriteLine(MENU);
+                response = int.Parse(Console.ReadLine());
+                switch(response)
+                {
+                    case 1:
+                        addBox();
+                        break;
+                    case 2:
+                        searchBoxes();
+                        break;
+                    case 3:
+                        browseBoxes();
+                        break;
+                    case 4:
+                        response = -1;
+                        break;
+                    default:
+                        break;
+                }
+            } while (response != -1);
+        }
+
+        static void addBox()
+        {
+            Console.WriteLine("What is in this box? seperate with spaces.");
+        }
+        static void searchBoxes()
+        {
+
+        }
+        static void browseBoxes()
+        {
+
         }
     }
 }
